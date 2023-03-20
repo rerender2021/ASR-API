c51eb66be3072ca2c9cdfe4a17aa87408ba4a98d
rerender2021 <rerender2021@outlook.com>
2023-03-20 23:02:50 +0800

    fix missing module

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 pyinstaller --noconfirm --clean .\config\main.spec
 
 Xcopy /Y /E /I .\venv\Lib\site-packages\vosk .\dist\ASR-API\vosk /EXCLUDE:.\exclude.txt
-Xcopy /Y /E /I .\venv\Lib\site-packages\numpy\distutils .\dist\ASR-API\numpy\distutils /EXCLUDE:.\exclude.txt
+Xcopy /Y /E /I .\venv\Lib\site-packages\transformers .\dist\ASR-API\transformers /EXCLUDE:.\exclude.txt
 Xcopy /Y /E /I .\model .\dist\ASR-API\model 
 
 copy /Y ".\script\run.bat" ".\dist\ASR-API\run.bat"
